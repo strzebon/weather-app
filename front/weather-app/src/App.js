eaa567c0ccd29f7ca3d77f82b80e4f9d41cdd252
Kacper Kotkiewicz <kapeczek32@gmail.com>
2023-12-12 11:20:31 +0100

    Small changes in main App

--- front/weather-app/src/App.js
+++ front/weather-app/src/App.js
@@ -1,6 +1,5 @@
 import Form from "./components/Form";
 import Navbar from "./components/Navbar";
-import WeatherOverview from "./components/WeatherOverview";
 import WeatherView from "./components/WeatherView";
 import "./styles/App.css"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
@@ -12,8 +11,7 @@ function App() {
         <div className="main-container">
           <BrowserRouter>
             <Routes>
-              {/* <Route path="/" element={<Form/>}></Route> */}
-              <Route path="/" element={<WeatherOverview location={"Cracow"} img_path={"day/113.png"} temp_c={5} condition={"Sunny"}/>}></Route>
+              <Route path="/" element={<Form/>}></Route>
               <Route path="/weather" element={<WeatherView/>}></Route>
             </Routes>
           </BrowserRouter>
