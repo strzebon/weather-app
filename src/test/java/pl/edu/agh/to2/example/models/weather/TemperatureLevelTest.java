9b2ba7c27b0ceb45a62de50aa1a057d5ea918233
Adam <Adam.ja.com@op.pl>
2023-12-12 22:14:24 +0100

    Test changed to parametrized

--- src/test/java/pl/edu/agh/to2/example/models/weather/TemperatureLevelTest.java
+++ src/test/java/pl/edu/agh/to2/example/models/weather/TemperatureLevelTest.java
@@ -1,66 +1,19 @@
 package pl.edu.agh.to2.example.models.weather;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static pl.edu.agh.to2.example.models.weather.TemperatureLevel.COLD;
-import static pl.edu.agh.to2.example.models.weather.TemperatureLevel.FREEZING;
-import static pl.edu.agh.to2.example.models.weather.TemperatureLevel.HOT;
-import static pl.edu.agh.to2.example.models.weather.TemperatureLevel.WARM;
 import static pl.edu.agh.to2.example.models.weather.TemperatureLevel.determineTemperatureLevel;
 
 class TemperatureLevelTest {
-
-    @Test
-    void shouldReturnFreezingTemperatureLevelWhenUnderTheirLevel() {
-        // given
-        double freezingTemperature = -15.0;
-        TemperatureLevel expectedLevel = FREEZING;
-
+    @ParameterizedTest
+    @CsvSource({"FREEZING, -15.0", "COLD, -2.0", "WARM, 12.0", "HOT, 23"})
+    void shouldReturnFreezingTemperatureLevelWhenUnderTheirLevel(TemperatureLevel expectedLevel, double temperature) {
         // when
-        TemperatureLevel actualLevel = determineTemperatureLevel(freezingTemperature);
+        TemperatureLevel actualLevel = determineTemperatureLevel(temperature);
 
         // then
         assertEquals(expectedLevel, actualLevel);
     }
-
-    @Test
-    void shouldReturnColdTemperatureLevelWhenUnderTheirLevel() {
-        // given
-        double freezingTemperature = -3.0;
-        TemperatureLevel expectedLevel = COLD;
-
-        // when
-        TemperatureLevel actualLevel = determineTemperatureLevel(freezingTemperature);
-
-        // then
-        assertEquals(expectedLevel, actualLevel);
-    }
-
-    @Test
-    void shouldReturnWarmTemperatureLevelWhenUnderTheirLevel() {
-        // given
-        double freezingTemperature = 12.0;
-        TemperatureLevel expectedLevel = WARM;
-
-        // when
-        TemperatureLevel actualLevel = determineTemperatureLevel(freezingTemperature);
-
-        // then
-        assertEquals(expectedLevel, actualLevel);
-    }
-
-    @Test
-    void shouldReturnHotTemperatureLevelWhenUnderTheirLevel() {
-        // given
-        double freezingTemperature = 25.0;
-        TemperatureLevel expectedLevel = HOT;
-
-        // when
-        TemperatureLevel actualLevel = determineTemperatureLevel(freezingTemperature);
-
-        // then
-        assertEquals(expectedLevel, actualLevel);
-    }
-
 }
