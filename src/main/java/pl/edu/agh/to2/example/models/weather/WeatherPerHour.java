a9929afb6a2b305a5fb049af01e3b2c92d9d2f62
Adam <Adam.ja.com@op.pl>
2024-01-16 16:28:32 +0100

    JsonProperty fix

--- src/main/java/pl/edu/agh/to2/example/models/weather/WeatherPerHour.java
+++ src/main/java/pl/edu/agh/to2/example/models/weather/WeatherPerHour.java
@@ -1,18 +1,18 @@
 package pl.edu.agh.to2.example.models.weather;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
 
 public record WeatherPerHour(
         String time,
-        @JsonProperty("temp_c")
+        @SerializedName("temp_c")
         double tempC,
-        @JsonProperty("precip_mm")
+        @SerializedName("precip_mm")
         double precipMm,
-        @JsonProperty("wind_kph")
+        @SerializedName("wind_kph")
         double windKph,
-        @JsonProperty("will_it_rain")
+        @SerializedName("will_it_rain")
         int willItRain,
-        @JsonProperty("will_it_snow")
+        @SerializedName("will_it_snow")
         int willItSnow
 ) {
 }
